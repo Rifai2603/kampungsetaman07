69aefd67851d1c9be9198b872d6a1ad7e96423e9
Ahmad Rifai <ahmad.rifa.te21@mhsw.pnj.ac.id>
2023-10-30 14:10:20 +0700

    kesatu

---
+++ app/pages/includes/footer.php
@@ -0,0 +1,79 @@
+<!-- Footer Start -->
+<div class="container-fluid page-header bg-primary text-light footer pt-5 mt-5" data-wow-delay="0.1s">
+        <div class="container py-5">
+            <div class="row g-5">
+                <div class="col-lg-4 col-md-6">
+                    <h4 class="text-white mb-3">Akses Cepat</h4>
+                    <a class="btn btn-link" style="text-decoration: none;" href="<?=ROOT?>/home">Home</a>
+                    <a class="btn btn-link" style="text-decoration: none;" href="<?=ROOT?>/about">About</a>
+                    <a class="btn btn-link" style="text-decoration: none;" href="<?=ROOT?>/blog">Blog</a>
+                    <a class="btn btn-link" style="text-decoration: none;" href="<?=ROOT?>/search">Search</a>
+                    <a class="btn btn-link" style="text-decoration: none;" href="<?=ROOT?>/login">Login</a>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <h4 class="text-white mb-3">Hubungi Kami</h4>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>Kampung Setaman, Jl. Rawa Geni Raya, RT.005/RW.007, Kel. Ratu Jaya, Kec. Cipayung, Kota Depok, Jawa Barat 16439</p>
+                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>0819-1245-4140</p>
+                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>kampungsetaman07@gmail.com</p>
+                    <div class="d-flex pt-2">
+                        <a class="btn btn-outline-light btn-social" href="https://www.instagram.com/kampungsetaman07/"><i class="fab fa-instagram"></i></a>
+                        <a class="btn btn-outline-light btn-social" href="mailto:kampungsetaman07@gmail.com"><i class="fa fa-envelope"></i></a>
+                        <a class="btn btn-outline-light btn-social" href="https://www.youtube.com/@kampungsetaman1661/featured"><i class="fab fa-youtube"></i></a>
+                        <a class="btn btn-outline-light btn-social" href="https://www.facebook.com/groups/649258509209142"><i class="fab fa-facebook-f"></i></a>
+                    </div> 
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <h4 class="text-white mb-3">Galeri</h4>
+                    <div class="row g-2 pt-2">
+                        <div class="col-3">
+                            <img class="img-fluid bg-light p-1" src="<?=ROOT?>/assets/images/doc1.jpg" alt="">
+                        </div>
+                        <div class="col-3">
+                            <img class="img-fluid bg-light p-1" src="<?=ROOT?>/assets/images/doc2.jpg" alt="">
+                        </div>
+                        <div class="col-3">
+                            <img class="img-fluid bg-light p-1" src="<?=ROOT?>/assets/images/doc3.jpg" alt="">
+                        </div>
+                        <div class="col-3">
+                            <img class="img-fluid bg-light p-1" src="<?=ROOT?>/assets/images/doc4.jpg" alt="">
+                        </div>
+                        <div class="col-3">
+                            <img class="img-fluid bg-light p-1" src="<?=ROOT?>/assets/images/doc5-1.jpg" alt="">
+                        </div>
+                        <div class="col-3">
+                            <img class="img-fluid bg-light p-1" src="<?=ROOT?>/assets/images/doc6.jpg" alt="">
+                        </div>
+                        <div class="col-3">
+                            <img class="img-fluid bg-light p-1" src="<?=ROOT?>/assets/images/doc7.jpg" alt="">
+                        </div>
+                        <div class="col-3">
+                            <img class="img-fluid bg-light p-1" src="<?=ROOT?>/assets/images/doc8.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="copyright">
+                <div class="row">
+                    <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                        &copy; <a class="border-bottom" style="text-decoration: none;" href="#">Kampung Setaman</a>, All Right Reserved.
+
+                        <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
+                        Designed By <a class="border-bottom" style="text-decoration: none;">PSBM-JTE-Politeknik Negeri Jakarta</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<!-- Footer End -->
+
+
+      <script src="<?=ROOT?>/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    </body>
+</html>
